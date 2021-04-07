ca505f7674c7809fbe04226ff12ddc477c2f8215
yash handa <yashhanda500@gmail.com>
2021-04-07 18:21:10 +0530

    update

--- src/components/login.js
+++ src/components/login.js
@@ -17,7 +17,7 @@ function login() {
                 </label><br></br>
                 <input type="submit" value="Submit" />
                 <br></br>
-                
+    {/* <Link to="./register ">If you are not a registered user, click here.</Link> */}
                 <a href="./register" login ><h5>If you are not a registered user, click here.</h5></a>
                 </div>
                 
