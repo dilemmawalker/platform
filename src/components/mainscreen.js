94a41226131aa9e30fc9da38d51ba433ba5a57cb
yash handa <yashhanda500@gmail.com>
2021-04-09 23:56:07 +0530

    update

--- src/components/mainscreen.js
+++ src/components/mainscreen.js
@@ -3,7 +3,15 @@ import React from 'react';
 function mainscreen(){
     return(
         <div>
-            <p>5-6 courses visible containing links of diff tests.</p>
+            <p>5-6 courses visible containing links of diff tests.
+            
+            </p>
+            <p>
+                MCA
+                B.Tech
+                B.E.//
+
+            </p>
         </div>
     );
 }
